fbe11a12b46f4139e4623f7081d3682ba73bc3b7
Karan Bhardwaj <91610499+karan00034@users.noreply.github.com>
2024-06-13 21:51:48 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -103,9 +103,11 @@ export default function CreatePost() {
             }
           >
             <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value='indie game'>Indie Games</option>
+            <option value='aaa games'>AAA Games</option>
+            <option value='open world games'>Open world games</option>
+            <option value='guide'>Game guide</option>
+            <option value='news'>News</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
